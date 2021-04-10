fb6cab8f13d8b811fc257b4ab59178a1f280f67e
Lee Warrick <leewarrick@gmail.com>
2021-04-10 11:30:01 -0400

    reorder folders

---
+++ challenges/001/javascript/problem/javascript/index.js
@@ -0,0 +1,14 @@
+mocha.setup('bdd')
+const expect = chai.expect
+
+function problem(param) {}
+
+describe('Problem Title', function() {
+  it('should return param', function() {
+    const param = 'banana'
+    const answer = problem(param)
+    expect(answer).to.eq(param)
+  })
+})
+
+mocha.run()
